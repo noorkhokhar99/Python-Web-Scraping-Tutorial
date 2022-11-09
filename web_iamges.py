8ac7ab8d3e83d09320923e33d12c0107965e00e0
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2022-11-09 12:00:21 +0500

    Add files via upload

---
+++ web_iamges.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# Making a GET request
+r = requests.get('https://uae.microless.com/')
+
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+
+images_list = []
+
+images = soup.select('img')
+for image in images:
+	src = image.get('src')
+	alt = image.get('alt')
+	images_list.append({"src": src, "alt": alt})
+	
+for image in images_list:
+	print(image)
